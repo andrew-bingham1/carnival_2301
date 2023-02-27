2699723f81e4955ec4da2eb84233a02d9f811b1b
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-27 10:56:22 -0700

    test: add most popular ride test

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -46,6 +46,57 @@ RSpec.describe Carnival do
     end
   end
 
+  describe '#most_popular_ride' do 
+    it 'can determine most popular ride' do 
+      carnival = Carnival.new(4)
+
+      ride1 = Ride.new({
+        name: 'Carousel',
+        min_height: 24,
+        admission_fee: 1,
+        excitement: :gentle
+        })
+
+      ride2 = Ride.new({
+        name: 'Ferris Wheel',
+        min_height: 36,
+        admission_fee: 5,
+        excitement: :gentle
+        })
+
+      ride3 = Ride.new({
+        name: 'Roller Coaster',
+        min_height: 54,
+        admission_fee: 2,
+        excitement: :thrilling
+        })
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor2 = Visitor.new('Tucker', 36, '$5')
+      visitor3 = Visitor.new('Penny', 64, '$15')
+
+      carnival.add_ride(ride1)
+      carnival.add_ride(ride2)
+      carnival.add_ride(ride3)
+
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+      visitor3.add_preference(:gentle)
+      visitor2.add_preference(:thrilling)
+     
+
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+      ride1.board_rider(visitor3)
+
+      ride2.board_rider(visitor1)
+      ride2.board_rider(visitor2)
+
+      ride3.board_rider(visitor2)
+require 'pry'; binding.pry
+      expect(carnival.most_popular_ride).to eq([ride1])
+    end
+  end
+
 
 
 end
