081b6605e43ff4562c93b4894a687b7098825742
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-27 11:03:28 -0700

    test: create most profitable ride test

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -96,6 +96,57 @@ RSpec.describe Carnival do
     end
   end
 
+  describe '#most_profitable_ride' do 
+    it 'can tell the most profitable ride' do
+      carnival = Carnival.new(4)
+
+      ride1 = Ride.new({
+        name: 'Carousel',
+        min_height: 24,
+        admission_fee: 1,
+        excitement: :gentle
+        })
+
+      ride2 = Ride.new({
+        name: 'Ferris Wheel',
+        min_height: 36,
+        admission_fee: 5,
+        excitement: :gentle
+        })
+
+      ride3 = Ride.new({
+        name: 'Roller Coaster',
+        min_height: 54,
+        admission_fee: 2,
+        excitement: :thrilling
+        })
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor2 = Visitor.new('Tucker', 36, '$5')
+      visitor3 = Visitor.new('Penny', 64, '$15')
+
+      carnival.add_ride(ride1)
+      carnival.add_ride(ride2)
+      carnival.add_ride(ride3)
+
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+      visitor3.add_preference(:gentle)
+      visitor2.add_preference(:thrilling)
+     
+
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+      ride1.board_rider(visitor3)
+
+      ride2.board_rider(visitor1)
+      ride2.board_rider(visitor2)
+
+      ride3.board_rider(visitor2)
+
+      expect(carnival.most_profitable_ride).to eq(ride2)
+    end
+  end
+
 
 
 end
