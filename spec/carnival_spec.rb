a60220c000ca02c58fbac67a9509e135856a26ee
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-27 10:39:47 -0700

    test: add add ride test

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -14,5 +14,38 @@ RSpec.describe Carnival do
     end
   end
 
+  describe '#add_ride' do
+    it 'can add rides' do
+      carnival = Carnival.new(4)
+      ride1 = Ride.new({
+        name: 'Carousel',
+        min_height: 24,
+        admission_fee: 1,
+        excitement: :gentle
+        })
+
+      ride2 = Ride.new({
+        name: 'Ferris Wheel',
+        min_height: 36,
+        admission_fee: 5,
+        excitement: :gentle
+        })
+
+      ride3 = Ride.new({
+        name: 'Roller Coaster',
+        min_height: 54,
+        admission_fee: 2,
+        excitement: :thrilling
+        })
+
+      carnival.add_ride(ride1)
+      carnival.add_ride(ride2)
+      carnival.add_ride(ride3)
+
+      expect(carnival.rides).to eq([ride1,ride2,ride3])
+    end
+  end
+
+
 
 end
